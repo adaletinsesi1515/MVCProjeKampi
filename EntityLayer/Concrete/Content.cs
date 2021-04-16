276f8fc6e72bb7d1856db128f9bd5837c5b8ca75
SÜLEYMAN YİĞİT GÜMEN <suleymanyigit.gumen@stu.bahcesehir.k12.tr>
2021-04-16 15:55:44 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        [Key]
+        public int ContentID { get; set; }
+        
+        [StringLength(1000)]
+        public string ContentValue { get; set; }
+        public DateTime ContentDate { get; set; }
+
+        
+        //Başlık ile İçerik alanı ile ilişki yaptık
+        public int HeadingID { get; set; }
+        public virtual Heading Heading { get; set; }
+
+        //İçerik ile Yazar alanını ilişki yaptık
+        public int WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
+    }
+}
