4f643a2a8ee94a166c9c44b08ec5e0c5646b482f
SÜLEYMAN YİĞİT GÜMEN <suleymanyigit.gumen@stu.bahcesehir.k12.tr>
2021-04-20 15:01:26 +0300

    20/04/2021 çalışması

---
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class CategoryRepository : IRepository<Category>
+    {
+        private Context db = new Context();
+        DbSet<Category> _object;
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        public void Insert(Category p)
+        {
+            _object.Add(p);
+            db.SaveChanges();
+        }
+
+        public void Delete(Category p)
+        {
+            _object.Remove(p);
+            db.SaveChanges();
+        }
+
+        public void Update(Category p)
+        {
+            db.SaveChanges();
+        }
+    }
+}
