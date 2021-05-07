2847508423131416f85313e9ab1ad69ef82afa1c
SÜLEYMAN YİĞİT GÜMEN <suleymanyigit.gumen@stu.bahcesehir.k12.tr>
2021-05-07 15:54:47 +0300

    TEMA VS GÜNCELLENDİ

--- MVCProjeKampi UI/Controllers/AdminCategoryController.cs
+++ MVCProjeKampi UI/Controllers/AdminCategoryController.cs
@@ -3,15 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 
 namespace MVCProjeKampi_UI.Controllers
 {
     public class AdminCategoryController : Controller
     {
         // GET: AdminCategory
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+
         public ActionResult Index()
         {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult result = categoryValidator.Validate(p);
+            if (result.IsValid)
+            {
+                cm.CategoryAddBL(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                    
+                }
+            }
+
+            return View();
+
+
+
+
         }
     }
 }
