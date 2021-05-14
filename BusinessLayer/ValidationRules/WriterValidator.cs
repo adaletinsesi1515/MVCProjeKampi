94e35f2e6c7308d4d73e9c07a78c20613e8de2e8
Memduh Gümen <75331205+adaletinsesi1515@users.noreply.github.com>
2021-05-15 01:46:54 +0300

    14 Mayıs dersi bitti

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adını boş geçemezsiniz!!!");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar soyadını boş geçemezsiniz!!!");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Yazar adını en az 2 karakter olacak şekilde giriniz!!!");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Yazar adını en fazla 50 karakter olacak şekilde giriniz!!!");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında alanını boş geçemezsiniz!!!");
+            //Burayı daha sonra büyük A harfini de kapsar şekilde güncellememiz lazım
+            RuleFor(x => x.WriterAbout).Matches("a").WithMessage("Hakkında alanında mutlaka a harfi geçmelidir.");
+            RuleFor(x => x.WriterMail).EmailAddress().WithMessage("Geçerli bir mail adresi giriniz");
+        }
+    }
+}
--- MVCProjeKampi UI/Controllers/WriterController.cs
+++ MVCProjeKampi UI/Controllers/WriterController.cs
@@ -1,7 +1,11 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,5 +22,46 @@ namespace MVCProjeKampi_UI.Controllers
             var listele = wm.GetList();
             return View(listele);
         }
+
+        [HttpGet]
+        public ActionResult WriterAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult WriterAdd(Writer p, HttpPostedFileBase WriterImage)
+        {
+            WriterValidator writerValidator = new WriterValidator();
+            ValidationResult result = writerValidator.Validate(p);
+            if (result.IsValid)
+            {
+                if (WriterImage.ContentLength>0)
+                {
+                    var image = Path.GetFileName(WriterImage.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Images"), image);
+                    WriterImage.SaveAs(path);
+                    p.WriterImage = "/Images/" + image;
+
+                }
+               
+                wm.WriterAddBL(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+
+
+
+
+
     }
 }
