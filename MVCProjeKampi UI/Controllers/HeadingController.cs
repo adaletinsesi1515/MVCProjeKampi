b7121de1916c20019397782607684ca45e0067c9
SÜLEYMAN YİĞİT GÜMEN <suleymanyigit.gumen@stu.bahcesehir.k12.tr>
2021-05-26 23:17:51 +0300

    Ders 48 e göre işlemler yapıldı

--- MVCProjeKampi UI/Controllers/HeadingController.cs
+++ MVCProjeKampi UI/Controllers/HeadingController.cs
@@ -5,18 +5,53 @@ using System.Web;
 using System.Web.Mvc;
 using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 
 namespace MVCProjeKampi_UI.Controllers
 {
     public class HeadingController : Controller
     {
-        private HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
 
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+
+        WriterManager wm = new WriterManager(new EfWriterDal());
         // GET: Heading
         public ActionResult Index()
         {
             var headingvalues = hm.GetList();
             return View(headingvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valuesCategory = (from x in cm.GetList()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryID.ToString()
+                                                   }).ToList();
+            ViewBag.vlcat = valuesCategory;
+
+            List<SelectListItem> valuesWriter = (from y in wm.GetList()
+                select new SelectListItem
+                {
+                    Text = y.WriterName+ " " +y.WriterSurname,
+                    Value = y.WriterID.ToString()
+                }).ToList();
+            ViewBag.vlwrt = valuesWriter;
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.HeadingAddBL(p);
+            return RedirectToAction("Index");
         }
     }
 }
