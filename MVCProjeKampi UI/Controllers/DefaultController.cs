c003f1bfc820adef34e8151187bb2561b6f79279
SYG <suleymanyigit.gumen@stu.bahcesehir.edu.tr>
2021-06-22 13:55:50 +0300

    81. ders bitti

---
+++ MVCProjeKampi UI/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjeKampi_UI.Controllers
+{
+    [AllowAnonymous]
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        public ActionResult Headings()
+        {
+            var listele = headingManager.GetList();
+            return View(listele);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
