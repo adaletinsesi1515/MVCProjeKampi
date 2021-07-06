3d9b9226165b264edbbad102becc8d265c4d5f0b
SYG <suleymanyigit.gumen@stu.bahcesehir.edu.tr>
2021-07-06 14:22:29 +0300

    grafikler

---
+++ MVCProjeKampi UI/Controllers/ChartController.cs
@@ -0,0 +1,51 @@
+﻿using MVCProjeKampi_UI.Sınıf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjeKampi_UI.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult CategoryChart()
+        {
+            return Json(Bloglist(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<CategoryChart> Bloglist()
+        {
+            List<CategoryChart> ct = new List<CategoryChart>();
+            ct.Add(new CategoryChart()
+            {
+                CategoryName = "Yazılım",
+                CategoryCount=8
+            });
+            ct.Add(new CategoryChart()
+            {
+                CategoryName = "Seyahat",
+                CategoryCount = 4
+            });
+            ct.Add(new CategoryChart()
+            {
+                CategoryName = "Teknoloji",
+                CategoryCount = 7
+            });
+            ct.Add(new CategoryChart()
+            {
+                CategoryName = "Spor",
+                CategoryCount = 1
+            });
+            return ct;
+        }
+
+    }
+}
---
+++ MVCProjeKampi UI/Sınıf/CategoryChart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProjeKampi_UI.Sınıf
+{
+    public class CategoryChart
+    {
+        public string CategoryName { get; set; }
+        public int CategoryCount { get; set; }
+    }
+}
